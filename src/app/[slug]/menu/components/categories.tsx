e9e389aebef50795e923fcaa348a026957abd8f8
gilberto-espinoso <gilbertosilvaespinosa@gmail.com>
2025-02-22 22:34:15 -0300

    feat: add ScrollArea and category buttons

--- src/app/[slug]/menu/components/categories.tsx
+++ src/app/[slug]/menu/components/categories.tsx
@@ -2,6 +2,9 @@ import { Prisma } from "@prisma/client";
 import { ClockIcon } from "lucide-react";
 import Image from "next/image";
 
+import { Button } from "@/components/ui/button";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+
 interface RestaurantCategoriesProps {
   restaurant: Prisma.RestaurantGetPayload<{
     include: { menuCategories: { include: { products: true } } };
@@ -10,25 +13,39 @@ interface RestaurantCategoriesProps {
 
 const RestaurantCategories = ({ restaurant }: RestaurantCategoriesProps) => {
   return (
-    <div className="relative z-50 -mt-6 rounded-t-3xl border bg-white p-5">
-      <div className="flex items-center gap-3">
-        <Image
-          src={restaurant.avatarImageUrl}
-          alt={restaurant.name}
-          width={45}
-          height={45}
-        />
+    <div className="relative z-50 -mt-6 rounded-t-3xl border bg-white">
+      <div className="p-5">
+        <div className="flex items-center gap-3">
+          <Image
+            src={restaurant.avatarImageUrl}
+            alt={restaurant.name}
+            width={45}
+            height={45}
+          />
 
-        <div>
-          <h2 className="text-lg font-semibold">{restaurant.name}</h2>
-          <p className="text-xs opacity-55">{restaurant.description}</p>
+          <div>
+            <h2 className="text-lg font-semibold">{restaurant.name}</h2>
+            <p className="text-xs opacity-55">{restaurant.description}</p>
+          </div>
+        </div>
+
+        <div className="mt-3 flex items-center gap-1 text-sm text-green-500">
+          <ClockIcon size={12} />
+          <p>Aberto!</p>
         </div>
       </div>
 
-      <div className="mt-3 flex items-center gap-1 text-sm text-green-500">
-        <ClockIcon size={12} />
-        <p>Aberto!</p>
-      </div>
+      <ScrollArea className="w-full">
+        <div className="flex w-max space-x-4 px-5 pb-4">
+          {restaurant.menuCategories.map((category) => (
+            <Button key={category.id} variant="secondary" size="sm">
+              {category.name}
+            </Button>
+          ))}
+        </div>
+
+        <ScrollBar orientation="horizontal" />
+      </ScrollArea>
     </div>
   );
 };
