0df0e63e1c3fac4c72d5062d6db544117b02ba06
gilberto-espinoso <gilbertosilvaespinosa@gmail.com>
2025-02-23 14:46:02 -0300

    feat: add context

---
+++ src/app/[slug]/context/cart.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { Product } from "@prisma/client";
+import { createContext, ReactNode, useState } from "react";
+
+interface ICartProduct extends Product {
+  quantity: number;
+}
+
+export interface ICartContext {
+  isOpen: boolean;
+  products: ICartProduct[];
+  toggleCart: () => void;
+}
+
+export const CartContext = createContext<ICartContext>({
+  isOpen: false,
+  products: [],
+  toggleCart: () => {},
+});
+
+export const CartProvider = ({ children }: { children: ReactNode }) => {
+  const [products, setProducts] = useState<ICartProduct[]>([]);
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleCart = () => setIsOpen((prev) => !prev);
+
+  return (
+    <CartContext.Provider value={{ isOpen, products, toggleCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
